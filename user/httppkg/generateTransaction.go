32ebbac162ed930828d1269d01d3e1252ccf9259
JINJAEHO <wogh126@gmail.com>
2022-07-26 18:42:59 +0900

    주석

--- user/httppkg/generateTransaction.go
+++ user/httppkg/generateTransaction.go
@@ -21,8 +21,8 @@ type JsonResponse struct {
 
 // Generate Transaction
 func (r *Request) ApplyCareer(w http.ResponseWriter, req *http.Request) {
-	Txs := txpkg.CreateTxDB() // 비어있는 TXS를 만들고-임시
-	Txid := Txs.AddTx(r.T)    // 그 비어있는 TXS안에 Transaction을 넣고 Txid를 반환
+	Txs := txpkg.CreateTxDB() // [임시]비어있는 TXS
+	Txid := Txs.AddTx(r.T)    // [임시]그 비어있는 TXS안에 Transaction을 넣고 Txid를 반환
 	var response = JsonResponse{Address: r.Address, Txid: Txid}
 	json.NewEncoder(w).Encode(response)
 }
